90591db2da64c3c934962bc047e8502c682dd66f
Ismaila Diallo <lodia.ismaila@gmail.com>
2021-02-08 00:40:29 +0100

    Add animation to modal

--- src/components/popup/index.js
+++ src/components/popup/index.js
@@ -1,31 +1,69 @@
-import React from 'react'
+import React, {useRef, useEffect, useCallback} from 'react'
+import { useSpring, animated } from "react-spring";
 import styled from 'styled-components'
 import { MdClose } from 'react-icons/md'
 import BookForm from '../bookForm'
 
 export const Modal = ({ showModal, setModal, bookEdit, setBookEdit }) => {
+  const modalRef = useRef();
+
+  const animation = useSpring({
+    config: {
+      duration: 250
+    },
+    opacity: showModal ? 1 : 0,
+    transform: showModal ? `translateY(0%)` : `translateY(-100%)`
+  });
+
+  const closeModal = e => {
+    if (modalRef.current === e.target) {
+      setModal(false);
+    }
+  };
+
+  const keyPress = useCallback(
+    e => {
+      if (e.key === 'Escape' && showModal) {
+        setModal(false);
+      }
+    },
+    [setModal, showModal]
+  );
+
+  useEffect(
+    () => {
+      document.addEventListener('keydown', keyPress);
+      return () => document.removeEventListener('keydown', keyPress);
+    },
+    [keyPress]
+  );
+
+  
   return (
     <>
       {showModal ? (
-        <Background>
+        <Background onClick={closeModal} ref={modalRef}>
           {
-            <ModalWrapper>
-              <ModalContent>
-                <div showModal={showModal}>
-                  <BookForm
-                    visible='true'
-                    bookEdit={bookEdit}
-                    setBookEdit={setBookEdit}
-                    showModal={showModal}
-                    setModal={setModal}
-                  ></BookForm>
-                  <CloseModalButton
-                    aria-label='Close modal'
-                    onClick={() => setModal(prev => !prev)}
-                  />
-                </div>
-              </ModalContent>
-            </ModalWrapper>
+            <animated.div style={animation}>
+              <ModalWrapper>
+                <ModalContent>
+                  <div showModal={showModal}>
+                    <BookForm
+                      visible='true'
+                      bookEdit={bookEdit}
+                      setBookEdit={setBookEdit}
+                      showModal={showModal}
+                      setModal={setModal}
+                    ></BookForm>
+                    <CloseModalButton
+                      aria-label='Close modal'
+                      onClick={() => setModal(prev => !prev)}
+                    />
+                  </div>
+                </ModalContent>
+              </ModalWrapper>
+            </animated.div>
+            
           }
         </Background>
       ) : null}
